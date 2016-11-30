787d6e99184af2459ac4cf677c02732a54b193ed
Timur Gibadullin <tim.gibadullin@gmail.com>
2016-12-01 02:49:14 +0300

    Define name of returning static component' class

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import {Image, Text, View} from 'react-native';
 
 
 export function createStaticComponent(BaseComponent) {
-    return class extends BaseComponent {
-        static displayName = `Static${BaseComponent.displayName}`;
+    const className = `Static${BaseComponent.name}`;
 
-        shouldComponentUpdate() {
-            return false;
-        }
-    };
+    return new Function(
+        'BaseComponent',
+        `class ${className} extends BaseComponent {
+            shouldComponentUpdate() {
+                return false;
+            }
+        };
+        ${className}.displayName = 'Static${BaseComponent.displayName}';
+        return ${className};`
+    )(BaseComponent);
 }
 
 export const StaticText = createStaticComponent(Text);
